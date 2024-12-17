f597ceec81403eeabc4efd1a93977740a51f1d82
OksanaOd <o_girich@ukr.net>
2024-12-17 20:20:04 +0200

    17/12/2024

--- Core3_Task_1.py
+++ Core3_Task_1.py
@@ -3,11 +3,16 @@ from datetime import datetime, date, timedelta
 #Hometask 1
 #розрахує кількість днів між заданою датою і поточною датою
 def string_to_date(date_string):
-    return datetime.strptime(date_string, "%Y.%m.%d").date()
+  print(date_string)
+  return datetime.strptime(date_string, "%Y-%m-%d").date()
 
 def get_days_from_today(date):
-  days_return = (datetime.today().date()-string_to_date(date)).days
+  try:
+    days_return = (datetime.today().date()-string_to_date(date)).days
+  except:
+    print("The input date is in an incorrect format")
+
   return days_return 
 
-days_from_today = get_days_from_today('2023.12.07')
+days_from_today = get_days_from_today('2024-13-07')
 print("кількість днів між заданою і поточною датою є", days_from_today)
