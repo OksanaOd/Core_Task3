9c1224b772cf42d7708793c05ea5078153d969a3
OksanaOd <o_girich@ukr.net>
2025-01-01 19:51:14 +0200

    Core_Task3

--- Core3_Task_1.py
+++ Core3_Task_1.py
@@ -6,11 +6,12 @@ def string_to_date(date_string):
   return datetime.strptime(date_string, "%Y-%m-%d").date()
 
 def get_days_from_today(date):
-  days_return = (datetime.today().date()-string_to_date(date)).days
-  return days_return 
+  try:
+    days_return = (datetime.today().date()-string_to_date(date)).days
+    return days_return 
+  except:
+    print("Вхідна дата має некоректний формат")   
   
-try:
-  days_from_today = get_days_from_today('2024-12-07')
+days_from_today = get_days_from_today('2024-12-07')
+if days_from_today != None:
   print("кількість днів між заданою і поточною датою є", days_from_today)
-except:
-  print("Вхідна дата має некоректний форммат")
--- Core3_Task_2.py
+++ Core3_Task_2.py
@@ -1,24 +1,35 @@
 import random
+
+min = int(input("Ввведить MIN , яке не менше 1 "))
+max = int(input("Ввведить MAX, яке не більше 1000 "))
+quantity = int(input("Ввведить quantity ")) 
 # Hometask 2
 #генерує набір унікальних випадкових чисел у межах заданих параметрів 
 def get_numbers_ticket(min, max, quantity):
   list_digitals = []
   i=0  
-  while i < quantity :
-    new_elem = random.randint(min,max)
-    if list_digitals.count(new_elem)==0:
+  
+  if min<1 or max>1000 or (max-min)<0 :
+    print("MIN та MAX повинні бути між 1 то 1000,та MAX повинен бути більше MIN ")
+  elif (max-min+1)<quantity :
+    print(f"при введених MIN та MAX , quantity має бути не більше {max-min+1}")
+  else:
+  #numbers_ticket=get_numbers_ticket(min, max, quantity)
+  #print("Випадкові числа:", numbers_ticket)
+    while i < quantity :
+      new_elem = random.randint(min,max)
+      if list_digitals.count(new_elem)==0:      
       #ця умова виключає неунікальні числф
-      list_digitals.append(new_elem)
-      i= i + 1
-  return sorted(list_digitals)    
+        list_digitals.append(new_elem)
+        i= i + 1
+    return sorted(list_digitals)    
 
-min = int(input("Ввведить MIN , яке не менше 1 "))
-max = int(input("Ввведить MAX, яке не більше 1000 "))
-quantity = int(input("Ввведить quantity ")) 
-if min<1 or max>1000 or (max-min)<0 :
-  print("MIN та MAX повинні бути між 1 то 1000,та MAX повинен бути більше MIN ")
-elif (max-min+1)<quantity :
-  print(f"при введених MIN та MAX , quantity має бути не більше {max-min+1}")
-else:
-  numbers_ticket=get_numbers_ticket(min, max, quantity)
+
+#if min<1 or max>1000 or (max-min)<0 :
+#  print("MIN та MAX повинні бути між 1 то 1000,та MAX повинен бути більше MIN ")
+#elif (max-min+1)<quantity :
+#  print(f"при введених MIN та MAX , quantity має бути не більше {max-min+1}")
+#else:
+numbers_ticket=get_numbers_ticket(min, max, quantity)
+if numbers_ticket != None:
   print("Випадкові числа:", numbers_ticket)
